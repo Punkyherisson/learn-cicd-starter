ebf5fb16ff3032ebec628cea313f25fef5773a39
Punkyherisson <emmanuel.goitia@gmail.com>
2025-01-29 22:42:52 +0100

    Fix security issues: add ReadHeaderTimeout and handle Write errors

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -91,6 +92,10 @@ func main() {
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: router,
+		ReadHeaderTimeout: 10 * time.Second,
+	}
+	if err = srv.ListenAndServe(); err != nil {
+		log.Fatal("Server error:", err)
 	}
 
 	log.Printf("Serving on port: %s\n", port)
